d9591f05473d411bd4ff61a3f66bed20157cf4ed
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-04-12 16:06:47 +0200

    Add the project Skeleton.

---
+++ Virtual Tourist/Virtual Tourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Gamal Gamaleldin on 4/12/19.
+//  Copyright © 2019 el3ankaboot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController : UIViewController {
+    
+}
---
+++ Virtual Tourist/Virtual Tourist/Model/FlickrClient.swift
@@ -0,0 +1,9 @@
+//
+//  FlickrClient.swift
+//  Virtual Tourist
+//
+//  Created by Gamal Gamaleldin on 4/12/19.
+//  Copyright © 2019 el3ankaboot. All rights reserved.
+//
+
+import Foundation
