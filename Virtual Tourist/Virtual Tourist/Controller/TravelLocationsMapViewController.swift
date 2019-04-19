d1160ba5c2b45057fa7875b99a35b84d814a1ff4
Gamal Gamaleldin <gamal.m.esmail@gmail.com>
2019-04-19 14:11:52 +0200

    Persist the center of the map.

--- Virtual Tourist/Virtual Tourist/Controller/TravelLocationsMapViewController.swift
+++ Virtual Tourist/Virtual Tourist/Controller/TravelLocationsMapViewController.swift
@@ -8,9 +8,50 @@
 
 import Foundation
 import UIKit
+import MapKit
 
-class TravelLocationsMapViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController , MKMapViewDelegate {
 
+    //MARK: Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: View did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.mapView.delegate = self
+        
+        if UserDefaults.standard.bool(forKey: "HasLaunchedBefore") {
+            print("App has launched before")
+            let latDouble = UserDefaults.standard.double(forKey: "Latitude")
+            let lat =  CLLocationDegrees(latDouble)
+            let longDouble = UserDefaults.standard.double(forKey: "Longitude")
+            let long = CLLocationDegrees(longDouble)
+            let center = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 20, longitudeDelta: 20))
+            
+            self.mapView.setRegion(region, animated: true)
+            
+        } else {
+            let startingCoordinate = mapView.centerCoordinate
+            let latitude = startingCoordinate.latitude
+            let longitude = startingCoordinate.longitude
+            print("This is the first launch ever!")
+            UserDefaults.standard.set(true, forKey: "HasLaunchedBefore")
+            UserDefaults.standard.set(longitude, forKey: "Longitude")
+            UserDefaults.standard.set(latitude, forKey: "Latitude")
+            UserDefaults.standard.set(0.0, forKey: "Zoom")
+            UserDefaults.standard.synchronize()
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = mapView.centerCoordinate
+        AppDelegate.longitude = center.longitude
+        AppDelegate.latitude = center.latitude
+    }
 
+    
 }
 
